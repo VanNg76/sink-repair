f6b9bf189560c1a215c1a5f803aeb5bf0b8e2494
Van Nguyen <thanh_xer@yahoo.com>
2022-02-01 12:07:09 -0600

    1st

---
+++ src/scripts/Requests.js
@@ -0,0 +1,31 @@
+import { getRequests } from "./dataAccess.js"
+import { deleteRequest } from "./dataAccess.js"
+
+export const Requests = () => {
+    const requests = getRequests()
+
+    let html = `
+        <ul>
+            ${
+                requests.map(request => {
+                    return `<li>${request.description}
+                    <button class="request__delete" id="request--${request.id}">
+                        Delete
+                    </button></li>`
+                }).join("")
+            }
+        </ul>
+    `
+
+    return html
+}
+
+// click event for Delete button
+const mainContainer = document.querySelector("#container")
+
+mainContainer.addEventListener("click", click => {
+    if (click.target.id.startsWith("request--")) {
+        const [,requestId] = click.target.id.split("--")
+        deleteRequest(parseInt(requestId))
+    }
+})
